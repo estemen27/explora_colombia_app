1951955fac54778497b9175a95412840cb021db1
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2024-11-21 19:19:55 -0500

    Integra el paquete artesanias al handmade fragment

--- app/src/main/java/com/estebanbe/exploracolombiaapp/fragments/TabFragments/HandMadeFragment.kt
+++ app/src/main/java/com/estebanbe/exploracolombiaapp/fragments/TabFragments/HandMadeFragment.kt
@@ -1,22 +1,47 @@
 package com.estebanbe.exploracolombiaapp.fragments.TabFragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.estebanbe.exploracolombiaapp.R
-
+import com.estebanbe.exploracolombiaapp.controller.artesanias.VisualizerArtesanias
 
 class HandMadeFragment : Fragment() {
+
+    private lateinit var imagenPrueba: ImageView
+    private lateinit var btnUbicacion: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.e("Fragmento!","HandMade")
-        return inflater.inflate(R.layout.fragment_hand_made, container, false)
-    }
+        // Inflar el diseño del fragmento
+        val view = inflater.inflate(R.layout.fragment_hand_made, container, false)
 
+        // Inicializar vistas
+        imagenPrueba = view.findViewById(R.id.main_img_1)
+        btnUbicacion = view.findViewById(R.id.btn_1)
+
+        // Configurar listeners
+        imagenPrueba.setOnClickListener {
+            // Abre la actividad VisualizerArtesanias
+            val intent = Intent(requireContext(), VisualizerArtesanias::class.java)
+            startActivity(intent)
+        }
+
+        btnUbicacion.setOnClickListener {
+            // Aquí puedes implementar la funcionalidad de geolocalización o cualquier otra acción
+            // Por ejemplo:
+            // val geoUri = Uri.parse("geo:0,0?q=Zipaquira, Colombia")
+            // val intent = Intent(Intent.ACTION_VIEW, geoUri)
+            // startActivity(intent)
+        }
+
+        return view
+    }
 }
